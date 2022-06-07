0e761d1dd9905533c471337b56f433dacde7710f
Scott Hammer <shammer64@gmail.com>
2022-06-07 19:40:26 -0400

    SH Partial practice session

--- src/components/ToDoListEntry/ToDoListEntry.js
+++ src/components/ToDoListEntry/ToDoListEntry.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ToDoListEntry.css';
 
-const ToDoListEntry = () => (
-  <div className="ToDoListEntry" data-testid="ToDoListEntry">
-    ToDoListEntry Component
-  </div>
-);
+const ToDoListEntry = ({onNewItem}) => {
+  const [newItem, setNewItem] = useState("");
+  
+  return (
+    <div className="ToDoListEntry" data-testid="ToDoListEntry">
+      <input type="text" placeholder='New Item' onChange={(e) => setNewItem(e.target.value)}/>
+      <button type='submit' disabled={!newItem} onSubmit={() => onNewItem(newItem)} >Add Item</button>
+    </div>
+  )
+};
 
 export default ToDoListEntry;
--- src/components/ToDoListEntry/ToDoListEntry.test.js
+++ src/components/ToDoListEntry/ToDoListEntry.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ToDoListEntry from './ToDoListEntry';
+import userEvent from '@testing-library/user-event';
 
 describe('<ToDoListEntry />', () => {
   test('it should mount', () => {
@@ -12,10 +13,48 @@ describe('<ToDoListEntry />', () => {
     expect(toDoListEntry).toBeInTheDocument();
   });
 
-  xtest("it should render a text input field", () => {});
-  xtest("it should render a submit button", () => {});
-  xtest("it should disable the submit button when no text is present", () => {});
-  xtest("it should enable the submit button when text is present", () => {});
-  xtest("it should execute a callback function with the input value as an argument when the submit button is pressed", () => {});
+  test("it should render a text input field", () => {
+    render(<ToDoListEntry />);
+    const textInput = screen.getByRole("textbox", {text: /New Item/i})
+
+    expect(textInput).toBeInTheDocument();
+  });
+
+  test("it should render a submit button", () => {
+    render(<ToDoListEntry />);
+    const submitButton = screen.getByRole("button", {text: /Add Todo/i})
+
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  test("it should disable the submit button when no text is present", () => {
+    render(<ToDoListEntry />);
+    const submitButton = screen.getByRole("button", { text: /Add Todo/i });
+
+    expect(submitButton).toBeDisabled()
+  });
+
+  test("it should enable the submit button when text is present", () => {
+    render(<ToDoListEntry />);
+    const submitButton = screen.getByRole("button", { text: /Add Todo/i });
+    const textInput = screen.getByRole("textbox", { text: /New Item/i });
+    
+    userEvent.type(textInput, 'Mow the Lawn');
+
+    expect(submitButton).toBeEnabled();
+  });
+
+  test("it should execute a callback function with the input value as an argument when the submit button is pressed", () => {
+    const callback = jest.fn((entry) => {});
+    render(<ToDoListEntry onNewItem={callback} />);
+    const submitButton = screen.getByRole("button", { text: /Add Todo/i });
+    const textInput = screen.getByRole("textbox", { text: /New Item/i });
+
+    userEvent.type(textInput, "Mow the Lawn");
+    expect(submitButton).toBeEnabled();
+    userEvent.click(submitButton);
+
+    expect(callback).toBeCalledTimes(1);
+  });
 
 });
